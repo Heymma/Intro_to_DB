587de42fcc144c7a9086b0de15751ee9e763563c
Ezeatu Emmanuel <heymma01@gmail.com>
2025-12-12 12:59:11 +0100

    Add task_3.sql

---
+++ task_3.sql
@@ -0,0 +1,9 @@
+-- List all tables in the current database.
+-- Provide the database name as an argument to the mysql client.
+-- Examples (PowerShell):
+--   mysql -u root -p alx_book_store < .\task_3.sql
+--   mysql -u root -p -D alx_book_store < .\task_3.sql
+
+USE alx_book_store;
+
+SHOW TABLES;
