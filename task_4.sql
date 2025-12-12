062bef1a04c207fe3ef69336f441aa0e005f1190
Ezeatu Emmanuel <heymma01@gmail.com>
2025-12-12 13:04:14 +0100

    Add task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+-- Show the full description of the table books in the current database.
+-- Provide the database name as an argument to the mysql client.
+-- Examples (PowerShell):
+--   mysql -u root -p alx_book_store < .\task_4.sql
+--   mysql -u root -p -D alx_book_store < .\task_4.sql
+
+SHOW CREATE TABLE books;
